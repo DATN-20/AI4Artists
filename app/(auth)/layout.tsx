cbbbb1e117ba1f756727afacbcd357ece7b6e1cd
20120265 <lixhen2002@gmail.com>
2024-02-02 23:39:22 +0700

    set up layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import { Footer, Navbar } from "@/components";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+}
