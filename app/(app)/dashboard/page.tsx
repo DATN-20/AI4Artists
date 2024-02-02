9235f7caf6caaf8ef6035252fdb61d6370315ce7
20120265 <lixhen2002@gmail.com>
2024-02-02 23:56:52 +0700

    add prettier and prettier tailwind

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,7 +1,9 @@
 export default function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
     </div>
   );
 }
--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -3,5 +3,5 @@ export default function AppLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return <>{children}</>;
 }
