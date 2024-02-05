c61d839b19d280ebaaf74f030aeb1e9fc4d4cf77
20120265 <lixhen2002@gmail.com>
2024-02-05 18:00:13 +0700

    feat: implement landing page

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,29 +1,33 @@
 "use client"
 
-import React, { useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { Button, Modal, Form, Input, Row, Col } from "antd";
-import { Store } from "antd/lib/form/interface";
-import { useAuth } from "./AuthProvider";
+import React, { useState } from "react"
+import Link from "next/link"
+import Image from "next/image"
+import { Button, Modal, Form, Input, Row, Col } from "antd"
+import { Store } from "antd/lib/form/interface"
+import { useAuth } from "./AuthProvider"
 
 const NavBar: React.FC = () => {
-  const { user, showModal } = useAuth();
+  const { user, showModal } = useAuth()
 
   return (
-    <header className="w-full  absolute z-10">
-      <nav className="mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-black">
-        <Link href="/" className="flex justify-center items-center">
-          <Image src="/logo.png" alt="logo" width={150} height={150} />
+    <header className="sticky z-10 w-full">
+      <nav className=" mx-auto flex items-center justify-between bg-[#0e1217] px-6 py-6 sm:px-12">
+        <Link href="/" className="flex items-center justify-center gap-4">
+          <Image src="/logo.png" alt="logo" width={60} height={60} />
+          <h1 className="text-3xl font-bold text-white">AI Artist</h1>
         </Link>
-        <Button
-          type="primary" onClick={showModal}
-          className="text-primary-blue rounded-full bg-black min-w-[130px] border border-white"
+        <button
+          type="button"
+          onClick={showModal}
+          className="flex min-w-[150px] items-center justify-center rounded-full border-[2px] border-white bg-[#0e1217] py-1"
         >
-          {user ? `Welcome, ${user}!` : "Sign in"}
-        </Button>
+          <h1 className="text-xl font-semibold">
+            {user ? `Welcome, ${user}!` : "Sign in"}
+          </h1>
+        </button>
       </nav>
     </header>
-  );
-};
-export default NavBar;
+  )
+}
+export default NavBar
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,11 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-linear": "linear-gradient(var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
+  important: true,
 }
 export default config
