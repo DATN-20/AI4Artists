2e5a44b1639a34490f66e75d07625a4d76759934
thanhlaunchdeck <thanh.le@launchdeck.org>
2024-02-26 15:13:24 +0700

    change ui

--- components/layout/ModalLogin.tsx
+++ components/layout/ModalLogin.tsx
@@ -1,203 +1,219 @@
-// "use client"
+"use client"
 
-// import React, { useEffect, useState } from "react"
-// import { Modal, Form, Input, Button, Divider, Row, Col } from "antd"
-// import { Store } from "antd/lib/form/interface"
+import React, { useEffect, useState } from "react"
+
 // import {
 //   GoogleOutlined,
 //   FacebookFilled,
 //   CheckCircleOutlined,
 // } from "@ant-design/icons"
-// import NextImage from "next/image"
-// import { useLoginUserMutation } from "@/services/authApi"
-// import { toast } from "react-toastify"
-// import { useAppDispatch } from "@/store/hooks"
-// import { setUser } from "@/features/authSlice"
+import NextImage from "next/image"
+import { useLoginUserMutation } from "@/services/authApi"
+import { toast } from "react-toastify"
+import { useAppDispatch } from "@/store/hooks"
+import { setUser } from "@/features/authSlice"
+import { Dialog, DialogContent } from "../ui/dialog"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
 
-// interface ModalLoginProps {
-//   open: boolean
-//   onClose: () => void
-//   setOpenRegister: (open: boolean) => void
-// }
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 
-// const ModalLogin: React.FC<ModalLoginProps> = ({
-//   open,
-//   onClose,
-//   setOpenRegister,
-// }) => {
-//   const dispatch = useAppDispatch()
-//   const [
-//     loginUser,
-//     {
-//       data: loginData,
-//       isSuccess: isLoginSuccess,
-//       isError: isLoginError,
-//       error: loginError,
-//     },
-//   ] = useLoginUserMutation()
-//   const handleSignUpClick = () => {
-//     onClose()
-//     setOpenRegister(true)
-//   }
-//   const [formData, setFormData] = useState({
-//     email: "",
-//     password: "",
-//   })
-//   const handleChange = (event: any) => {
-//     setFormData({ ...formData, [event.target.name]: event.target.value })
-//   }
-//   const handleSubmit = async (values: Store) => {
-//     if (formData.email && formData.password) {
-//       await loginUser({ email: formData.email, password: formData.password })
-//     } else {
-//       toast.error("Please fill all Input field")
-//     }
-//   }
+const formSchema = z.object({
+  email: z.string().min(2, {
+    message: "Email must be at least 2 characters.",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+})
 
-//   useEffect(() => {
-//     if (isLoginSuccess) {
-//       toast.success("User login successfully")
-//       dispatch(setUser({ token: loginData.token, name: loginData.name }))
-//     }
-//   }, [isLoginSuccess])
+interface ModalLoginProps {
+  open: boolean
+  onClose: () => void
+  setOpenRegister: (open: boolean) => void
+}
 
-//   return (
-//     <Modal
-//       title=""
-//       open={open}
-//       onOk={() => {}}
-//       onCancel={onClose}
-//       width={1000}
-//       destroyOnClose
-//       footer={null}
-//       className="modal-login"
-//     >
-//       <Row gutter={16}>
-//         <Col
-//           span={12}
-//           className=" rounded-l-md bg-gradient-to-r from-fuchsia-900 to-fuchsia-400"
-//         >
-//           <div
-//             className="flex flex-col items-center"
-//             style={{ padding: "30px" }}
-//           >
-//             <NextImage
-//               alt="logo"
-//               width={200}
-//               height={200}
-//               src="/logo-login.png"
-//             ></NextImage>
-//             <div className="mt-5 text-2xl font-bold text-white ">
-//               <div className="flex">
-//                 <CheckCircleOutlined />{" "}
-//                 <p className="ml-5">Over 50 Free Image Generations Daily</p>
-//               </div>
+const ModalLogin: React.FC<ModalLoginProps> = ({
+  onClose,
+  setOpenRegister,
+}) => {
+  const dispatch = useAppDispatch()
+  const [
+    loginUser,
+    {
+      data: loginData,
+      isSuccess: isLoginSuccess,
+      isError: isLoginError,
+      error: loginError,
+    },
+  ] = useLoginUserMutation()
 
-//               <br />
-//               <div className="flex">
-//                 <CheckCircleOutlined />{" "}
-//                 <p className="ml-5">Over 50 Free Image Generations Daily</p>
-//               </div>
-//               <br />
-//               <div className="flex">
-//                 <CheckCircleOutlined />{" "}
-//                 <p className="ml-5">Over 50 Free Image Generations Daily</p>
-//               </div>
-//               <br />
-//               <div className="flex">
-//                 <CheckCircleOutlined />{" "}
-//                 <p className="ml-5">Over 50 Free Image Generations Daily</p>
-//               </div>
-//             </div>
-//           </div>
-//         </Col>
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
 
-//         <Col span={8} push={2} className="pt-10">
-//           <Form onFinish={(values: Store) => handleSubmit(values)}>
-//             <h1 className="mb-5 text-3xl font-bold">Welcome Back</h1>
-//             <Form.Item
-//               label="Email"
-//               name="email"
-//               tooltip="Example@email.com"
-//               rules={[
-//                 { required: true, message: "Please input your email!" },
-//                 {
-//                   type: "email",
-//                   message: "Please enter a valid email address!",
-//                 },
-//               ]}
-//               labelCol={{ span: 24 }}
-//               wrapperCol={{ span: 24 }}
-//             >
-//               <Input value={formData.email} onChange={handleChange} />
-//             </Form.Item>
-//             <Form.Item
-//               label="Password"
-//               name="password"
-//               tooltip="At least 8 characters"
-//               rules={[
-//                 { required: true, message: "Please input your password!" },
-//                 {
-//                   min: 8,
-//                   message: "Password must be at least 8 characters!",
-//                 },
-//               ]}
-//               labelCol={{ span: 24 }}
-//               wrapperCol={{ span: 24 }}
-//             >
-//               <Input.Password
-//                 value={formData.password}
-//                 onChange={handleChange}
-//               />
-//             </Form.Item>
-//             <Form.Item style={{ marginBottom: 0 }}>
-//               <a href="#" className="text-primary-blue float-right">
-//                 Forget Password?
-//               </a>
-//             </Form.Item>
-//             <Form.Item className="mt-5 ">
-//               <Button
-//                 type="primary"
-//                 htmlType="submit"
-//                 className="h-10 w-full bg-black"
-//               >
-//                 Sign In
-//               </Button>
-//             </Form.Item>
+  const handleSignUpClick = () => {
+    onClose()
+    setOpenRegister(true)
+  }
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+  const handleChange = (event: any) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value })
+  }
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    if (formData.email && formData.password) {
+      await loginUser({ email: formData.email, password: formData.password })
+    } else {
+      toast.error("Please fill all Input field")
+    }
+  }
 
-//             {/* Divider and Social Media Buttons */}
-//             <Divider plain style={{ borderTopColor: "#cfccd2" }}>
-//               Or
-//             </Divider>
-//             <Button
-//               block
-//               icon={<GoogleOutlined />}
-//               className="mb-5 h-10 w-full bg-red-600 text-white"
-//             >
-//               Sign in with Google
-//             </Button>
-//             <Button
-//               block
-//               icon={<FacebookFilled />}
-//               className="mb-5 h-10 w-full bg-blue-800 text-white"
-//             >
-//               Sign in with Facebook
-//             </Button>
-//             <p className="mb-10 text-center">
-//               Don't you have an account?{" "}
-//               <a
-//                 href="#"
-//                 className="text-primary-blue"
-//                 onClick={handleSignUpClick}
-//               >
-//                 Sign up
-//               </a>
-//             </p>
-//           </Form>
-//         </Col>
-//       </Row>
-//     </Modal>
-//   )
-// }
+  // useEffect(() => {
+  //   if (isLoginSuccess) {
+  //     toast.success("User login successfully")
+  //     dispatch(setUser({ token: loginData.token, name: loginData.name }))
+  //   }
+  // }, [isLoginSuccess])
 
-// export default ModalLogin
+  return (
+    <DialogContent className="min-w-[950px] p-0">
+      <div className="flex ">
+        <div className=" mr-10 rounded-l-md bg-gradient-to-r from-fuchsia-900 to-fuchsia-400">
+          <div
+            className="flex flex-col items-center"
+            style={{ padding: "30px" }}
+          >
+            <NextImage
+              alt="logo"
+              width={200}
+              height={200}
+              src="/logo-login.png"
+            ></NextImage>
+            <div className="mt-5 text-2xl font-bold text-white ">
+              <div className="mt-5 flex">
+                {/* <CheckCircleOutlined />{" "} */}
+                <p className="ml-5">Over 50 Free Image Generations Daily</p>
+              </div>
+
+              <br />
+              <div className="mt-5 flex">
+                {/* <CheckCircleOutlined />{" "} */}
+                <p className="ml-5">Over 50 Free Image Generations Daily</p>
+              </div>
+              <br />
+              <div className="mt-5 flex">
+                {/* <CheckCircleOutlined />{" "} */}
+                <p className="ml-5">Over 50 Free Image Generations Daily</p>
+              </div>
+              <br />
+              <div className="mt-5 flex">
+                {/* <CheckCircleOutlined />{" "} */}
+                <p className="ml-5">Over 50 Free Image Generations Daily</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-80 pt-5">
+          <Form {...form}>
+            <h1 className="mb-5 text-center text-3xl font-bold">
+              Welcome Back
+            </h1>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="Email"
+                        {...field}
+                        value={formData.email}
+                        onChange={handleChange}
+                        className="w-full"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Password"
+                        {...field}
+                        type="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <a href="#" className="text-primary-blue float-right">
+                Forget Password?
+              </a>
+              <Button type="submit" className="h-10 w-full bg-black">
+                Sign In
+              </Button>
+
+              {/* Divider and Social Media Buttons */}
+              <Separator style={{ borderTopColor: "#cfccd2" }}></Separator>
+              <Button
+                // icon={<GoogleOutlined />}
+                className="h-10 w-full bg-red-600 text-white"
+              >
+                Sign in with Google
+              </Button>
+              <Button
+                // icon={<FacebookFilled />}
+                className="h-10 w-full bg-blue-800 text-white"
+              >
+                Sign in with Facebook
+              </Button>
+              <div className="mb-10 text-center">
+                Don't you have an account?{" "}
+                <a
+                  href="#"
+                  className="text-primary-blue"
+                  onClick={handleSignUpClick}
+                >
+                  Sign up
+                </a>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </DialogContent>
+  )
+}
+
+export default ModalLogin
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import { ModalLogin, ModalRegister } from "@/components/layout"
 import { useAppSelector } from "@/store/hooks"
 import { selectAuth } from "@/features/authSlice"
+import { Dialog, DialogTrigger } from "../ui/dialog"
 const NavBar: React.FC = () => {
   const [openLogin, setOpenLogin] = useState(false)
   const [openRegister, setOpenRegister] = useState(false)
@@ -13,34 +14,41 @@ const NavBar: React.FC = () => {
   return (
     <header className="sticky z-10 w-full">
       <nav className=" mx-auto flex items-center justify-between bg-[#0e1217] px-6 py-6 sm:px-12">
-        <Link href="/" className="flex items-center justify-center gap-4">
-          <Image src="/logo.png" alt="logo" width={60} height={60} />
-          <h1 className="text-3xl font-bold text-white">AI Artist</h1>
-        </Link>
-        <div className="flex">
-          <button
-            type="button"
-            onClick={() => setOpenLogin(true)}
-            className="mr-10 flex min-w-[150px] items-center justify-center rounded-full border-[2px] border-white bg-white py-1"
-          >
-            {!name ? (
-              <h1 className="text-xl font-semibold">Sign in </h1>
-            ) : (
-              `Welcome, ${name}`
-            )}
-          </button>
-        </div>
+        <Dialog>
+          <Link href="/" className="flex items-center justify-center gap-4">
+            <Image src="/logo.png" alt="logo" width={60} height={60} />
+            <h1 className="text-3xl font-bold text-white">AI Artist</h1>
+          </Link>
+          <div className="flex">
+            <DialogTrigger asChild>
+              <button
+                type="button"
+                className="mr-10 flex min-w-[150px] items-center justify-center rounded-full border-[2px] border-white bg-white py-1"
+              >
+                <h1 className="text-xl font-semibold">Sign in </h1>
+              </button>
+            </DialogTrigger>
+            <DialogTrigger asChild>
+              <button
+                type="button"
+                className="mr-10 flex min-w-[150px] items-center justify-center rounded-full border-[2px] border-white bg-white py-1"
+              >
+                <h1 className="text-xl font-semibold">Sign up </h1>
+              </button>
+            </DialogTrigger>
+          </div>
 
-        {/* <ModalLogin
-          open={openLogin}
-          setOpenRegister={setOpenRegister}
-          onClose={() => setOpenLogin(false)}
-        />
-        <ModalRegister
-          open={openRegister}
-          setOpenLogin={setOpenLogin}
-          onClose={() => setOpenRegister(false)}
-        /> */}
+          <ModalLogin
+            open={openLogin}
+            setOpenRegister={setOpenRegister}
+            onClose={() => setOpenLogin(false)}
+          />
+          <ModalRegister
+            open={openRegister}
+            setOpenLogin={setOpenLogin}
+            onClose={() => setOpenRegister(false)}
+          />
+        </Dialog>
       </nav>
     </header>
   )
