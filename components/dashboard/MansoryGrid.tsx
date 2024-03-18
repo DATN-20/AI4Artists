ee9fa0014731f223b69c67c57acedc19c1fbe276
20120265 <lixhen2002@gmail.com>
2024-03-18 22:09:13 +0700

    feat: fix mansory grid

--- components/dashboard/MansoryGrid.tsx
+++ components/dashboard/MansoryGrid.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react"
 import { useGetAllDashboardImageQuery } from "@/services/dashboard/dashboardApi"
 import Loading from "../Loading"
 import { DashboardImage } from "@/types/dashboard"
+
 const MasonryGrid = ({ type }: { type: string }) => {
   const { data, error, isLoading } = useGetAllDashboardImageQuery({
     type: type,
@@ -23,9 +24,19 @@ const MasonryGrid = ({ type }: { type: string }) => {
     return <div>No images found.</div>
   }
 
+  // Distributing images in a round-robin fashion
+  const numberOfColumns = 5
+  const imageColumns = Array.from({ length: numberOfColumns }, () => [])
+
+  data.data.forEach((item, index) => {
+    const columnIndex = index % numberOfColumns
+    imageColumns[columnIndex].push(item)
+  })
+
   const renderColumn = (images: DashboardImage[]) => {
     return images.map((item, index) => (
       <img
+        key={index} // Add a key here for React to handle re-renders efficiently
         className="h-auto w-full rounded-lg"
         src={item.image.url}
         alt={item.image.prompt}
@@ -33,15 +44,10 @@ const MasonryGrid = ({ type }: { type: string }) => {
     ))
   }
 
-  const chunkSize = Math.ceil(data.data.length / 5)
-  const imageColumns = Array.from({ length: 5 }, (_, i) =>
-    data.data.slice(i * chunkSize, i * chunkSize + chunkSize),
-  )
-
   return (
     <div className="mt-4 grid grid-cols-2 gap-4 md:grid-cols-5">
       {imageColumns.map((column, index) => (
-        <div className=" flex flex-col gap-4" key={index}>
+        <div className="flex flex-col gap-4" key={index}>
           {renderColumn(column)}
         </div>
       ))}
