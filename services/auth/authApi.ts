5951255d056d996c120231ef5536e4333ac4d05e
hungvanle321 <hungvanle321@gmail.com>
2024-06-16 11:30:19 +0700

    fix:API port

--- services/auth/authApi.ts
+++ services/auth/authApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/",
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/",
   }),
   endpoints: (builder) => ({
     loginUser: builder.mutation({
