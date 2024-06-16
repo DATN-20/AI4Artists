25c0dd2765337db71da0acaafd63416422beccaa
20120265 <lixhen2002@gmail.com>
2024-06-16 13:07:52 +0700

    feature: fix search dashboard

--- components/dashboard/DashboardContent.tsx
+++ components/dashboard/DashboardContent.tsx
@@ -9,7 +9,10 @@ import {
 } from "../ui/dropdown-menu"
 import { useEffect, useState, useRef, useCallback } from "react"
 import MansoryGrid from "./MansoryGrid"
-import { useGetAllDashboardImageQuery } from "@/services/dashboard/dashboardApi"
+import {
+  useGetAllDashboardImageQuery,
+  useGetSearchImageQuery,
+} from "@/services/dashboard/dashboardApi"
 import { DashboardImage } from "@/types/dashboard"
 import Loading from "../Loading"
 
@@ -30,6 +33,8 @@ const DashboardContent: React.FC = () => {
   const [images, setImages] = useState<DashboardImage[]>([])
   const [page, setPage] = useState<number>(1)
   const [showSmallLoading, setShowSmallLoading] = useState<boolean>(false)
+  const [searchQuery, setSearchQuery] = useState<string>("")
+  const [searchResults, setSearchResults] = useState<DashboardImage[]>([])
   const limit = 20
 
   const isFetchingRef = useRef<boolean>(false)
@@ -48,6 +53,15 @@ const DashboardContent: React.FC = () => {
     limit,
   })
 
+  const { data: searchData, refetch: refetchSearch } = useGetSearchImageQuery(
+    {
+      query: searchQuery,
+      page: 1,
+      limit: limit,
+    },
+    { skip: !searchQuery },
+  )
+
   useEffect(() => {
     if (error) {
       console.error("Failed to fetch images:", error)
@@ -56,6 +70,12 @@ const DashboardContent: React.FC = () => {
       isFetchingRef.current = false
     }
   }, [error, data])
+
+  useEffect(() => {
+    if (searchData) {
+      setSearchResults(searchData.data)
+    }
+  }, [searchData])
 
   const handleScroll = useCallback(() => {
     if (!containerRef.current || isLoading || isFetchingRef.current) return
@@ -78,6 +98,16 @@ const DashboardContent: React.FC = () => {
       return () => currentContainer.removeEventListener("scroll", handleScroll)
     }
   }, [handleScroll])
+
+  useEffect(() => {
+    if (searchQuery) {
+      refetchSearch()
+    }
+  }, [searchQuery, refetchSearch])
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value)
+  }
 
   return (
     <div
@@ -113,6 +143,8 @@ const DashboardContent: React.FC = () => {
           <input
             type="text"
             placeholder="Search"
+            value={searchQuery}
+            onChange={handleSearchChange}
             className="flex-grow bg-transparent outline-none dark:placeholder-white"
           />
           <Search />
@@ -124,7 +156,7 @@ const DashboardContent: React.FC = () => {
           <Loading />
         </div>
       ) : (
-        <MansoryGrid data={images} />
+        <MansoryGrid data={searchQuery ? searchResults : images} />
       )}
 
       {showSmallLoading && (
