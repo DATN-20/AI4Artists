7e8fb5d07498ab3a7433bfb8ac1fc79776db5268
thanhlaunchdeck <thanh.le@launchdeck.org>
2024-06-19 13:00:26 +0700

    feat: implement more info in toast

--- store/socketContext.tsx
+++ store/socketContext.tsx
@@ -1,8 +1,23 @@
 // WebSocketContext.tsx
-import React, { createContext, useContext, useEffect, useRef } from "react"
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react"
 import { io, Socket } from "socket.io-client"
 import { toast } from "react-toastify"
 import createSocket from "../services/socket"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { useGetNotificationImageQuery } from "@/services/generate/generateApi"
+import { ErrorObject } from "@/types"
+import NotificationImage from "@/components/layout/NotificationDialog"
 
 interface WebSocketContextProps {
   socket: Socket | null
@@ -17,7 +32,68 @@ export const WebSocketProvider: React.FC<{
   token: string
   children: React.ReactNode
 }> = ({ userId, token, children }) => {
+  const [referenceData, setReferenceData] = useState<string>("")
+  const [open, setOpen] = useState(false)
+
   const socketRef = useRef<Socket | null>(null)
+  const { data: notificationImage, refetch } = useGetNotificationImageQuery(
+    referenceData || "",
+  )
+
+  const notificationDisplayTime = (createdAt: string) => {
+    const date = new Date(createdAt)
+    return date.toLocaleString()
+  }
+
+  const toggleRead = async (reference_data: any) => {
+    if (reference_data) {
+      setReferenceData(reference_data)
+      const result = await refetch()
+      if ((result as ErrorObject).error) {
+        toast.error((result as ErrorObject).error.data.message)
+      }
+    }
+  }
+
+  const renderNotificationContent = (notification: any) =>
+    notification.reference_data ? (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogHeader className="hidden" />
+        <DialogTrigger className="my-3 flex w-full text-start">
+          <div
+            className={`flex items-center gap-4 border-b p-4 text-gray-800 hover:cursor-pointer hover:bg-slate-300 dark:text-white dark:hover:bg-gray-800`}
+            onClick={toggleRead}
+          >
+            <div>
+              <h4 className="font-bold">{notification.title}</h4>
+              <p>{notification.content}</p>
+              <span className={`text-xs text-primary`}>
+                {notificationDisplayTime(notification.created_at)}
+              </span>
+              <div className={`h-[10px] w-[15px] rounded-full`}></div>
+            </div>
+          </div>
+        </DialogTrigger>
+        <DialogContent className="max-h-[90vh] overflow-y-scroll sm:max-w-[80vw] md:max-w-[60vw]">
+          {notificationImage && (
+            <NotificationImage
+              style={notificationImage?.style || ""}
+              prompt={notificationImage?.prompt || ""}
+              images={notificationImage?.images || []}
+            />
+          )}
+        </DialogContent>
+      </Dialog>
+    ) : (
+      <div>
+        <h4 className="font-bold">{notification.title}</h4>
+        <p>{notification.content}</p>
+        <span className={`text-xs text-primary`}>
+          {notificationDisplayTime(notification.created_at)}
+        </span>
+        <div className={`h-[10px] w-[15px] rounded-full`}></div>
+      </div>
+    )
 
   useEffect(() => {
     if (!socketRef.current) {
@@ -33,8 +109,9 @@ export const WebSocketProvider: React.FC<{
 
       // Listen for notifications
       // Improve in the future, css the Toast
+
       socket.on("notification", (notification) => {
-        toast.info(`New notification: ${notification.title}`)
+        toast.info(renderNotificationContent(notification))
       })
 
       socketRef.current = socket
